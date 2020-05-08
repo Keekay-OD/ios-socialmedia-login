11c79cbe2add65c23bf3e67b568cec33917f8bce
OmardTech <omardiaz2012@gmail.com>
2020-05-07 22:42:16 -0700

    Added Main files

---
+++ homebuyer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homebuyer
+//
+//  Created by Omar Diaz on 5/4/20.
+//  Copyright © 2020 Omar Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
